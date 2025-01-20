41c55bc5496dd638456de8f5a3db170ac54285dd
ayushpatwal011 <patwalayush2005@gmail.com>
2025-01-20 21:25:18 +0530

    C:/Program Files/Git/page completed

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Pokemon from "./Pokemon";
+
+export default function App() {
+  return (
+   <div>
+    <Pokemon />
+   </div>
+  )
+}
---
+++ src/Pokemon.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import PokemonCard from "./PokemonCards";
+import logo from "./assets/pokeball.svg";
+import image from "./assets/search.svg";
+
+const Pokemon = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState("");
+
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=124";
+
+  useState(() => {
+    const fetchApi = async () => {
+      try {
+        const res = await fetch(API);
+        const data = await res.json();
+
+        const pokemonDetails = data.results.map(async (poke) => {
+          const res = await fetch(poke.url);
+          const pokeData = await res.json();
+          return pokeData;
+        });
+        const detailsResponse = await Promise.all(pokemonDetails);
+        setPokemon(detailsResponse);
+      } catch (error) {
+        setError(error);
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchApi();
+  }, []);
+
+  const searchData = pokemon.filter((poke) => {
+    const name = poke.name.toLowerCase();
+    const searchLower = search.toLowerCase();
+  
+    // Check if the `name` and `search` match character by character
+    for (let i = 0; i < searchLower.length; i++) {
+      if (name[i] !== searchLower[i]) {
+        return false;
+      }
+    }
+    return true;
+  });
+  
+  
+  
+  
+  if (loading) {
+    return (
+      <div className="w-full h-screen flex justify-center items-center bg-gray-200">
+        <h1 className="text-black font-extrabold text-5xl">Loading...</h1>
+      </div>
+    );
+  } else {
+    if (error) {
+      return (
+        <div className="w-full h-screen flex justify-center items-center bg-gray-200">
+          <h1 className="text-black font-extrabold text-lg md:text-5xl">
+            {error.message}
+          </h1>
+        </div>
+      );
+    } else {
+      return (
+        <main>
+          <nav className="bg-[#dc0a2d] p-4 px-8 shadow-md">
+            <div className="container mx-auto flex flex-col gap-5 ">
+              {/* Logo */}
+              <div className="flex items-center space-x-4">
+                <img src={logo} alt="Pokedex Logo" className="h-10 w-10" />
+                <span className="text-2xl font-extrabold text-white ">
+                  {"POKEMON CARDS"}
+                  <p className="text-sm">createdBy: ayushpatwal</p>
+                </span>
+              </div>
+
+              {/* Search Bar */}
+              <div className="flex px-4 bg-gray-100 rounded-2xl">
+                <p className="flex justify-center items-center">
+                  <img src={image} alt="search img" className="w-4 h-4" />
+                </p>
+                <input
+                  type="text"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                  placeholder="Search Pokemon..."
+                  className="w-full p-2  border-none focus:outline-none  bg-gray-100"
+                />
+              </div>
+            </div>
+          </nav>
+
+          <div className="w-full h-full ">
+            <ul className=" pt-10 px-10 flex flex-wrap justify-center gap-6 bg-gray-100">
+              {searchData.map((poke, i) => {
+                return <PokemonCard key={i} poke={poke} />;
+              })}
+            </ul>
+          </div>
+        </main>
+      );
+    }
+  }
+};
+export default Pokemon;
