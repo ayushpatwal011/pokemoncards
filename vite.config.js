9f031231cd14a698bff18a70d98410670f07276f
ayush patwal <137012714+ayushpatwal011@users.noreply.github.com>
2025-01-22 11:19:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
+// vite.config.mjs
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'react-router-dom': require.resolve('react-router-dom')
-    }
+      'react-router-dom': require.resolve('react-router-dom'),
+    },
   },
   build: {
     rollupOptions: {
